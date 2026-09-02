7e9affde0d0164fdc696a531bf3d6b9c3e0cd0de
rohitrot738 <araj5945@gmail.com>
2026-09-02 20:47:44 +0530

    Integrate complete 1000-stock engine in execution/__init__.py

--- execution/__init__.py
+++ execution/__init__.py
@@ -2,5 +2,6 @@
 
 from .batch_engine import BatchReport, iter_batches, run_batches
 from .worker_pool import WorkerPoolReport, run_workers
+from .async_fetch import fetch_concurrently
 
-__all__ = ["BatchReport", "iter_batches", "run_batches", "WorkerPoolReport", "run_workers"]
+__all__ = ["BatchReport", "iter_batches", "run_batches", "WorkerPoolReport", "run_workers", "fetch_concurrently"]
