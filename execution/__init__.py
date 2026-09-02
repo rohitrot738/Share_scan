dd1f4feb62acf24d400554cf3021afc6f03572d0
rohitrot738 <araj5945@gmail.com>
2026-09-02 20:35:28 +0530

    Add execution/__init__.py

---
+++ execution/__init__.py
@@ -0,0 +1,5 @@
+"""Execution helpers for stable full-universe scans."""
+
+from .batch_engine import BatchReport, iter_batches, run_batches
+
+__all__ = ["BatchReport", "iter_batches", "run_batches"]
