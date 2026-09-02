93e8b0ef222f1fab02ee093cc3e096bfdd202e17
rohitrot738 <araj5945@gmail.com>
2026-09-02 20:38:54 +0530

    Connect bounded worker pool in execution/__init__.py

--- execution/__init__.py
+++ execution/__init__.py
@@ -1,5 +1,6 @@
 """Execution helpers for stable full-universe scans."""
 
 from .batch_engine import BatchReport, iter_batches, run_batches
+from .worker_pool import WorkerPoolReport, run_workers
 
-__all__ = ["BatchReport", "iter_batches", "run_batches"]
+__all__ = ["BatchReport", "iter_batches", "run_batches", "WorkerPoolReport", "run_workers"]
